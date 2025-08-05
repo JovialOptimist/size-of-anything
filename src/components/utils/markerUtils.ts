40661175a7ad7f44e9c02ba7f46b0dfbc8f065d4
Jovialist <jac@thinkman.com>
2025-08-05 14:26:44 -0700

    Better rendering for markers
    Markers no longer have their tops cut off.

--- src/components/utils/markerUtils.ts
+++ src/components/utils/markerUtils.ts
@@ -13,13 +13,13 @@ export function createMarker(center: L.LatLng, color: string = "blue"): L.Marker
     title: "Drag to move area",
     icon: L.divIcon({
       className: "area-marker",
-      html: `<svg width="${width}" height="${height}" viewBox="0 0 18 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      html: `<svg width="${width}" height="${height}" viewBox="-1 -1 19 25" fill="none" xmlns="http://www.w3.org/2000/svg">
         <ellipse cx="9" cy="8" rx="7" ry="7" fill="${color}" stroke="white" stroke-width="2"/>
         <path d="M9 23C9 23 16 13.5 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 13.5 9 23 9 23Z" fill="${color}" stroke="white" stroke-width="2"/>
         <circle cx="8" cy="8" r="3" fill="white"/>
       </svg>`,
       iconSize: [width, height],
-      iconAnchor: [9 * markerSize, 23 * markerSize],
+      iconAnchor: [width / 2 + 1, height],
     }),
   });
 }
